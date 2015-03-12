82947ddbd76ede7cb01232a4994981abc933f536
关振东 <zhendongguan@gmail.com>
2015-03-12 10:23:04 +0800

    js文件
    之前代码格式化有错误，现恢复过来了

--- showInfo.js
+++ showInfo.js
@@ -2,15 +2,25 @@ var xmlHttp
 
 function showInfo() {
     var name
-    var grade name = document.getElementById("name").value grade = document.getElementById("grade").value
+    var grade 
+    name = document.getElementById("name").value 
+    grade = document.getElementById("grade").value
     if (name.length == 0) {
         document.getElementById("result").innerHTML = ""
         return
     }
-    xmlHttp = GetXmlHttpObject() if (xmlHttp == null) {
-        alert("浏览器不支持请求！！！") return
+    xmlHttp = GetXmlHttpObject() 
+    if (xmlHttp == null) {
+        alert("浏览器不支持请求！！！") 
+        return
     }
-    var url = "search.php"url = url + "?name=" + name url = url + "&grade=" + grade url = url + "&sid=" + Math.random() xmlHttp.onreadystatechange = stateChanged xmlHttp.open("GET", url, true) xmlHttp.send(null)
+    var url = "search.php"
+    url = url + "?name=" + name 
+    url = url + "&grade=" + grade 
+    url = url + "&sid=" + Math.random() 
+    xmlHttp.onreadystatechange = stateChanged 
+    xmlHttp.open("GET", url, true) 
+    xmlHttp.send(null)
 }
 
 function stateChanged() {
